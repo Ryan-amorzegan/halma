e20336b595bae2b4e8d429314cec5699c287fcf6
unknown <arianamorzegan@gmail.comgit config --global user.name Ryan-amorzegan>
2020-12-18 22:32:04 +0330

    chidane mohre haye p1 va bazi bi ghanon

--- boarder.c
+++ boarder.c
@@ -1,10 +1,15 @@
 #include "stdio.h"
-void print_board( int zel){
+void print_board(int game[50][50], int zel){
+     for (int j = 0; j <= 2* zel; j++)
+        {
+            printf("%c",61);
+        }
+    printf("\n");
     for (int i = 0; i < zel; i++)
     {
         for (int j = 0; j < zel; j++)
         {
-            printf("%c ",186);
+            printf("%c%c",186,game[i][j]);
         }
         printf("%c\n",186);
         for (int j = 0; j <= 2* zel; j++)
@@ -14,10 +19,45 @@ void print_board( int zel){
         printf("\n");
         
 
-    }
-    return 0;    
+    }   
 }
 int main(){
-    print_board(10);
+    int board[50][50];
+    int zel;
+    scanf("%d",&zel);
+    for (int i = 0; i < zel; i++)
+    {
+        for (int j = 0; j < zel; j++)
+        {
+            board[i][j] = 32;
+        }
+        
+    }
+    board[0][0] = 220;
+    board[0][1] = 220;
+    board[0][2] = 220;
+    board[0][3] = 220;
+    board[2][0] = 220;
+    board[3][1] = 220;
+    board[2][2] = 220;
+    board[3][0] = 220;
+    board[2][1] = 220;
+    board[1][2] = 220;
+    board[1][2] = 220;
+    board[1][0] = 220;
+    board[1][1] = 220;
+    board[1][3] = 220;
+    print_board(board,zel);
+    while (1)
+    {
+        int i,j,x,y;
+        scanf("%d %d",&i,&j);
+        board[i-1][j-1] = 32;
+        scanf("%d %d",&x,&j);
+        board[x - 1][y - 1] = 220;
+        printf("%c",board[x - 1][y - 1]);
+        print_board(board,zel);
+    }
+    
     return 0;
 }
