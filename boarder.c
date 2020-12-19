fad72e313d569910d54eda4f7e68d99fb1ab8ef0
unknown <arianamorzegan@gmail.comgit config --global user.name Ryan-amorzegan>
2020-12-20 03:01:45 +0330

    tamiz kari va possible moves

--- boarder.c
+++ boarder.c
@@ -21,33 +21,39 @@ void print_board(int game[50][50], int zel){
 
     }   
 }
-void posible(int board[50][50], int a,int x,int y){
-    x = x - 1;
-    y = y - 1;
-    if (a)
-    {
-        for (int i = -1; i <=1; i++)
-        {
-            for (int j = -1; j <= 1; j++)
-            {
-                if (x + i >= 0 && y + j >= 0)
-                {
-                    if (board[x+ i][y + j] == 32)
-                    {
-                        board[x+ i][y + j] = 42;
-                    }   
-                }
-                
-            }
-            
-        }
-        
+int fasele(int x,int y,int i,int j){        /* x o y makane avalie i o j makane sanavie*/
+    if ((i - x)*(i - x) + (j - y)*(j -  y) <= 2){
+        return 0;
     }
     else
     {
-        for (int i = 0; i < x; i++)
+        return 1;
+    }
+    
+}
+void posible(int board[50][50],int x,int y){ /* int a age 1 bashe makan haye momken ro neshon mide age 0 bashe board ro reset mikone*/
+    x = x - 1;
+    y = y - 1;
+    for (int i = -1; i <=1; i++)
         {
-            for (int j = 0; j < y; j++)
+        for (int j = -1; j <= 1; j++)
+        {
+            if (x + i >= 0 && y + j >= 0)
+            {
+                if (board[x+ i][y + j] == 32)
+                {
+                    board[x+ i][y + j] = 42;
+                }   
+            }
+                
+        }    
+    }
+
+}
+int reset(int board[50][50], int zel){
+     for (int i = 0; i < zel; i++)
+        {
+            for (int j = 0; j < zel; j++)
             {
                 if (board[i][j] == 42)
                 {
@@ -57,10 +63,6 @@ void posible(int board[50][50], int a,int x,int y){
             }
             
         }
-        
-    }
-    
-    
 }
 int main(){
     int board[50][50];
@@ -84,7 +86,6 @@ int main(){
     board[3][0] = 220;
     board[2][1] = 220;
     board[1][2] = 220;
-    board[1][2] = 220;
     board[1][0] = 220;
     board[1][1] = 220;
     board[1][3] = 220;
@@ -94,13 +95,13 @@ int main(){
 		int i, j,x,y;
 		printf("\nyour move \n");
 		scanf("%d %d", &i, &j);
-		printf("to which position??");
-        posible(board,1,i,j);
+        posible(board,i,j);
         print_board(board, zel);
+        printf("to which position??\n");
 		scanf("%d %d", &x, &y);
 		board[x - 1][y - 1] = 223;
 		board[i - 1][j - 1] = 32;
-        posible(board,0,zel,zel);
+        reset(board,zel);
     }
     
     return 0;
